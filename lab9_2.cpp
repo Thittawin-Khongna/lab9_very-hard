21c44af7600dff119201df3e7a804d5cec557bba
Thittawin <ptkk00177@gmail.com>
2024-12-17 14:07:23 +0700

    lab9_2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO (int x,int y){
+	if(x <= 0 || y <= 0 ){
+		cout<<"Invalid input";
+	}
+	else{
+		for (int i = 0;i<x;i++){
+			for(int j = 0;j<y;j++){
+				cout<<"O";
+			}
+		cout<<"\n";
+		}
+	}
+}
 
 int main(){
 	
 	printO(2,2);
 	cout << "\n";
-	
+
 	printO(3,5);
 	cout << "\n";	
 	
@@ -19,6 +31,8 @@ int main(){
 	
 	printO(7,-1);
 	cout << "\n";	
+	
+	
 	
 	return 0;
 }
